29cd5d6fd09ab9964d796ecf5d31bb83366f851c
George Gathogo <gathogogeorge11@gmail.com>
2024-05-09 10:47:42 +0300

    first commit

---
+++ src/components/Item.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+function Item({ name, category }) {
+
+  const [added, setIsOn] = useState(false);
+
+function handlePush(){
+  setIsOn((added) => !added);
+}
+
+  return (
+    <li className={added ? "in-cart" : ""}>
+      <span>{name}</span>
+      <span className="category">{category}</span>
+      <button className={added ? "remove": "add"} onClick={handlePush}>{added ? "Remove from Cart" : "Add to Cart"}</button>
+    </li>
+  );
+}
+
+export default Item;
---
+++ src/components/ShoppingList.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import Item from "./Item";
+
+function ShoppingList({ items }) {
+
+  const [selectedCategory, setCategory] = useState("All")
+ 
+  
+
+  function handleChange(e){
+    setCategory(e.target.value);
+    
+  }
+
+  
+  return (
+    <div className="ShoppingList">
+      <div className="Filter">
+        <select onChange={handleChange} name="filter">
+          <option value="All">Filter by category</option>
+          <option value="Produce">Produce</option>
+          <option value="Dairy">Dairy</option>
+          <option value="Dessert">Dessert</option>
+        </select>
+      </div>
+      <ul className="Items">
+        {items.filter((item)=>{
+          if( selectedCategory === "All"){
+          return item;
+        }
+          return selectedCategory === item.category;
+  }).map((item) => (
+          <Item key={item.id} name={item.name} category={item.category} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ShoppingList;
